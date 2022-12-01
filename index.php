fb8a37bf3d987f917112372eef2314173d0ed31e
Titouan Pastor <titouan.pastor@gmail.com>
2022-12-01 21:32:19 +0100

    Maquette V1

--- index.php
+++ index.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
@@ -7,40 +8,49 @@
     <title>SexInfo | SIS ASSOCIATION</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
+
 <body>
     <header>
         <ul id="menu">
-            <li><img src="imG/imageTest.jpg"></li>
-            <li><a href ="">page1</a></li>
-            <li><a href ="">page2</a></li>
-            <li><a href ="">page3</a></li>
+            <li><a href="#"><img src="imG/imageTest.jpg"></a></li>
+            <li><a href="#info1">info1</a></li>
+            <li><a href="#info2">info2</a></li>
+            <li><a href="#info3">info3</a></li>
         </ul>
     </header>
     <main>
-        <div id = "jeu">
-            <p>test</p>
-        </div>
-        <div id = "information">
-            <div id="info1">
-            <img src="imG/imageTest.jpg">
-                
+        <section id="jeu">
+        </section>
+        <section id="information">
+            <article id="info1" class="information-article">
+                <h1>Informations 1</h1>
+                <div class="contenu">
+                    <img src="img/imageTest.jpg" alt="">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi tempora iure dignissimos placeat suscipit nostrum error voluptatem, dolor non architecto eum delectus distinctio veritatis minima mollitia eligendi quam maxime aliquid!</p>
+                </div>
+            </article>
+            <article id="info2" class="information-article reverse">
+                <h1>Informations 2</h1>
+                <div class="contenu">
+                    <img src="img/imageTest.jpg" alt="">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi tempora iure dignissimos placeat suscipit nostrum error voluptatem, dolor non architecto eum delectus distinctio veritatis minima mollitia eligendi quam maxime aliquid!</p>
+                </div>
+            </article>
+            <article id="info3" class="information-article">
+                <h1>Informations 3</h1>
+                <div class="contenu">
+                    <img src="img/imageTest.jpg" alt="">
+                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi tempora iure dignissimos placeat suscipit nostrum error voluptatem, dolor non architecto eum delectus distinctio veritatis minima mollitia eligendi quam maxime aliquid!</p>
+                </div>
+            </article>
 
-            </div>
-            <div id="info2">
-            <img src="imG/imageTest.jpg">
-
-            </div>
-            <div id="info3">
-            <img src="imG/imageTest.jpg">
-
-            </div>
-        
-        </div>
+        </section>
 
     </main>
     <footer>
-        <p></p>
+        <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aliquam tempore saepe, aspernatur nihil cupiditate doloremque. Inventore earum incidunt, aperiam quo, doloremque, eligendi beatae ab maxime adipisci architecto atque fugiat dolorem?</p>
     </footer>
-    
+
 </body>
+
 </html>
