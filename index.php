731de0465fc2f2e0bca28d83077afd5529a26892
Titouan Pastor <titouan.pastor@gmail.com>
2022-12-02 00:41:17 +0100

    Ajout du konamiCode

--- index.php
+++ index.php
@@ -19,6 +19,7 @@
         </ul>
     </header>
     <main>
+        <span class="infokonami">info</span>
         <section id="jeu">
         </section>
         <section id="information">
@@ -52,5 +53,98 @@
     </footer>
 
 </body>
+<script>
+    checkKonami = 0;
+    const UP = '38';
+    const DOWN = '40';
+    const RIGHT = '39';
+    const LEFT = '37';
+    const A = '65';
+    const B = '66';
+
+    document.addEventListener('keydown', (event) => {
+        const touche = event.keyCode;
+        konamiTab = [UP, UP, DOWN, DOWN, LEFT, RIGHT, LEFT, RIGHT, B, A];
+
+        switch (checkKonami) {
+            case 0:
+                if (touche == UP) {
+                    checkKonami++;
+                }
+                break;
+            case 1:
+                if (touche == UP) {
+                    checkKonami++;
+                } else {
+                    checkKonami = 0;
+                }
+                break;
+            case 2:
+                if (touche == DOWN) {
+                    checkKonami++;
+
+                } else {
+                    checkKonami = 0;
+                }
+                break;
+            case 3:
+                if (touche == DOWN) {
+                    checkKonami++;
+                } else {
+                    checkKonami = 0;
+                }
+                break;
+            case 4:
+                if (touche == LEFT) {
+                    checkKonami++;
+                } else {
+                    checkKonami = 0;
+                }
+                break;
+            case 5:
+                if (touche == RIGHT) {
+                    checkKonami++;
+                } else {
+                    checkKonami = 0;
+                }
+                break;
+            case 6:
+                if (touche == LEFT) {
+                    checkKonami++;
+                } else {
+                    checkKonami = 0;
+                }
+                break;
+            case 7:
+                if (touche == RIGHT) {
+                    checkKonami++;
+                } else {
+                    checkKonami = 0;
+                }
+                break;
+            case 8:
+                if (touche == B) {
+                    checkKonami++;
+                } else {
+                    checkKonami = 0;
+                }
+                break;
+            case 9:
+                if (touche == A) {
+                    //KONAMI CODE FAIT !!!!!
+                    // A IMPLEMENTER
+                    document.querySelector('.infokonami').innerHTML = "KONAMI CODE FAIT !!!!!";
+                    checkKonami = 0;
+                } else {
+                    checkKonami = 0;
+                }
+                break;
+            default:
+                checkKonami = 0;
+                break;
+        }
+        console.log(checkKonami);
+    });
+</script>
 
 </html>
