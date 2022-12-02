d6ff75bb5d5ec3485c848095295b0c04eb412a18
Titouan Pastor <titouan.pastor@gmail.com>
2022-12-02 01:37:27 +0100

    On commence les cadeaux

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>SexInfo | SIS ASSOCIATION</title>
+    <title>SidaCaca | SIS ASSOCIATION</title>
     <link rel="stylesheet" href="css/style.css">
 </head>
 
@@ -19,7 +19,19 @@
         </ul>
     </header>
     <main>
-        <span class="infokonami">info</span>
+
+        <img class="cadeau" id="cadeau1" src="img/cadeau.png" alt="Un cadeau">
+        <img class="cadeau" id="cadeau2" src="img/cadeau.png" alt="Un cadeau">
+        <img class="cadeau" id="cadeau3" src="img/cadeau.png" alt="Un cadeau">
+        <img class="cadeau" id="cadeau4" src="img/cadeau.png" alt="Un cadeau">
+        <img class="cadeau" id="cadeau5" src="img/cadeau.png" alt="Un cadeau">
+        <img class="cadeau" id="cadeau6" src="img/cadeau.png" alt="Un cadeau">
+        <img class="cadeau" id="cadeau7" src="img/cadeau.png" alt="Un cadeau">
+        <img class="cadeau" id="cadeau8" src="img/cadeau.png" alt="Un cadeau">
+        <img class="cadeau" id="cadeau9" src="img/cadeau.png" alt="Un cadeau">
+        <img class="cadeau" id="cadeau10" src="img/cadeau.png" alt="Un cadeau">
+
+        <span class="infokonami"></span>
         <section id="jeu">
         </section>
         <section id="information">
@@ -81,6 +93,8 @@
 
 </body>
 <script>
+    // KONAMI CODE
+
     checkKonami = 0;
     const UP = '38';
     const DOWN = '40';
@@ -176,8 +190,87 @@
                 checkKonami = 0;
                 break;
         }
-        console.log(checkKonami);
     });
+
+    // EASTEREGG CADEAU
+
+    // A METTRE DANS TOUTES LES PAGES CONTENANT DES CADEAUX
+    // LA SESSION S'APPELLE "cadeaux"
+    // LES IDs DES CADEAUX DOIVENT S'APPELER "cadeauX"
+
+    var kdo1 = document.getElementById("cadeau1");
+    kdo1.addEventListener("click", sessionCadeaux("cadeau1"));
+    document.getElementById("cadeau2").addEventListener("click", sessionCadeaux("cadeau2"));
+    document.getElementById("cadeau3").addEventListener("click", sessionCadeaux("cadeau3"));
+    document.getElementById("cadeau4").addEventListener("click", sessionCadeaux("cadeau4"));
+    document.getElementById("cadeau5").addEventListener("click", sessionCadeaux("cadeau5"));
+    document.getElementById("cadeau6").addEventListener("click", sessionCadeaux("cadeau6"));
+    document.getElementById("cadeau7").addEventListener("click", sessionCadeaux("cadeau7"));
+    document.getElementById("cadeau8").addEventListener("click", sessionCadeaux("cadeau8"));
+    document.getElementById("cadeau9").addEventListener("click", sessionCadeaux("cadeau9"));
+    document.getElementById("cadeau10").addEventListener("click", sessionCadeaux("cadeau10"));
+
+    tabCadeaux = [false, false, false, false, false, false, false, false, false, false]
+
+    function sessionCadeaux(idDuCadeau) {
+        console.log("azfaf")
+        if (localStorage.getItem("cadeaux")) {
+
+            tabCheck = localStorage.getItem("cadeaux");
+            check = 0;
+
+            while (tabCheck[i] == true) {
+                check++;
+            }
+            if (check == 9) {
+                // implémenter l'action a faire quand le joueur a 10 cadeaux
+            }
+
+            switch (idDuCadeau) {
+                case "cadeau1":
+                    tab[0] = true;
+                    localStorage.setItem("cadeaux", tabCadeaux);
+                    break;
+                case "cadeau2":
+                    tab[1] = true;
+                    localStorage.setItem("cadeaux", tabCadeaux);
+                    break;
+                case "cadeau3":
+                    tab[2] = true;
+                    localStorage.setItem("cadeaux", tabCadeaux);
+                    break;
+                case "cadeau4":
+                    tab[3] = true;
+                    localStorage.setItem("cadeaux", tabCadeaux);
+                    break;
+                case "cadeau5":
+                    tab[4] = true;
+                    localStorage.setItem("cadeaux", tabCadeaux);
+                    break;
+                case "cadeau6":
+                    tab[5] = true;
+                    localStorage.setItem("cadeaux", tabCadeaux);
+                    break;
+                case "cadeau7":
+                    tab[6] = true;
+                    localStorage.setItem("cadeaux", tabCadeaux);
+                    break;
+                case "cadeau8":
+                    tab[7] = true;
+                    localStorage.setItem("cadeaux", tabCadeaux);
+                    break;
+                case "cadeau9":
+                    tab[8] = true;
+                    localStorage.setItem("cadeaux", tabCadeaux);
+                    break;
+                case "cadeau10":
+                    tab[9] = true;
+                    localStorage.setItem("cadeaux", tabCadeaux);
+                    break;
+            }
+
+        }
+    }
 </script>
 
 </html>
